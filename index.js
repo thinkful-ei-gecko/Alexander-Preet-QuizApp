ca3a42f35a7a8c6da8ee1cfbe6c00591d7817303
github username <github email address>
2019-08-18 21:47:06 -0500

    functions and HTML updated, CSS commented out in the HTML file, updated DATASTORE.js to include the assessment portion.

--- index.js
+++ index.js
@@ -34,25 +34,24 @@ function startQuiz(){
   }));
 }
 
-//Generate question, a callback function for nextQuestion().
-function generateQuestionHtml(number){
-  let questionData, answerHtml, totalHtml;
-  questionData = morbidQuestions[number];
-  questionData.answers.push(questionData.correctAnswer);
-  shuffle(questionData.answers);
-  answerHtml = generateAnswerHtml(questionData.answers);
-  totalHtml = `
-  <section class="question">
-      <p class="questionText">${questionData.question}</p>
-  </section>
-  <section class="answers">
-    <ul>${answerHtml}</ul>
-  </section>
-  `;
-  return totalHtml;
- }
+//generate questions into HTML, changed from original 
+ function generateQuestionHtml(){
+  const answer1 = `${morbidQuestions[questionNum].answers[0]}`;
+  const answer2 = `${morbidQuestions[questionNum].answers[1]}`;
+  const answer3 = `${morbidQuestions[questionNum].answers[2]}`;
+  const answer4 = `${morbidQuestions[questionNum].answers[3]}`;
+  const questionText = `<legend>${questionNum+1}/6: ${morbidQuestions[questionNum].question}<legend>`;
+  const answersText = 
+  `<input type='radio' name='option' class='radio-buttons' id='answer1' value='${answer1}'><label for='answer1'>${answer1}</label><br>
+  <input type='radio' name='option' class='radio-buttons' id='answer2' value='${answer2}'><label for='answer2'>${answer2}</label><br>
+  <input type='radio' name='option' class='radio-buttons' id='answer3' value='${answer3}'><label for='answer3'>${answer3}</label><br>
+  <input type='radio' name='option' class='radio-buttons' id='answer4' value='${answer4}'><label for='answer4'>${answer4}</label><br>`;
+  $('.historyQuestion').html(questionText);
+  $('.historyAnswers').html(answersText);
+  submitButton();
+}
 
-//Write html string for each answer
+//Write html string for each answer - may need to change this portion of the code, to fit in with above. 
 function generateAnswerHtml(answerArr){
   let answers;
   answerArr.map((answer )=> answers += `
@@ -60,6 +59,20 @@ function generateAnswerHtml(answerArr){
   `).join();
   return answers;
 }
+
+// restore submit button after disabling it for next questions
+function submitButton() {
+  
+  $('input[name=option]').on('click', function(event) {
+    $('.submitAnswer').removeClass('disabled').removeAttr('disabled');
+  });
+}
+
+
+
+
+
+
 
 //next question
 function nextQuestion(){
@@ -77,6 +90,8 @@ function nextQuestion(){
 })
  
 
+
+
 // submit selected answer
 function submitAnswer() {
   $('.submitAnswer').click(function(event) {
@@ -86,6 +101,9 @@ function submitAnswer() {
     $('.submitAnswer').hide();
   });
 }
+
+
+
 //check if answer is correct/incorrect, display correct answer and updated score
 let userScore = {
   correct: 0,
@@ -106,6 +124,7 @@ function evaluateUserAnswers() {
 }  
 
 
+
 //final page with the final score 
 function renderResults(){
   $('.submitAnswer').show(); 
@@ -115,6 +134,8 @@ function renderResults(){
   $('.finalCorrect').append(finalScoreText);
 
 }
+
+
 
 //restart quiz, user clicks back to the home page 
 function restartQuiz() {
@@ -128,4 +149,5 @@ $(generateQuestionHtml());
 $(submitAnswer());
 $(nextQuestion());
 $(restartQuiz());
+$(submitButton());
 
