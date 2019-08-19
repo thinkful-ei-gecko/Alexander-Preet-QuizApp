1e4fa75321b9ca9714ecc6a620da34ba72e07034
Alexander Fukui <alexander.fukui@gmail.com>
2019-08-19 02:34:37 -0500

    integrating some elements from Singh

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-'use strict'
+'use strict';
 
 //This should probably be in a function later.
 let questionNum = 0;
+let userScore = {
+  correct: 0,
+  incorrect: 0,
+};
+let quizStart = '';
 
 /* Shuffles an array based on Fisher-Yates algorithm:
 * https://gomakethings.com/how-to-shuffle-an-array-with-vanilla-js/
@@ -29,7 +34,7 @@ function shuffle(arr){
 //Start quiz, set questionNum, retrieve first question.
 function startQuiz(){
   $('.startButton').on('click', (event => {
-    $('.quizStart, .quizAbout').remove();
+    quizStart = $('.quizStart, .quizAbout').detach();
     nextQuestion(questionNum);
   }));
 }
@@ -52,67 +57,121 @@ function generateQuestionHtml(number){
       <p class="questionText">${questionData.question}</p>
   </section>
   <section class="answers">
-    <ul>${answerHtml}</ul>
+    <form class='historyForm'>
+      <fieldset name='answerSet'>
+      ${answerHtml}
+      <button type='button' class='submitAnswer' disabled> Check Answer </button>
+      </fieldset>
+    </form>
   </section>
   `;
   return totalHtml;
 }
 
 function generateAnswerHtml(answerArr){
-  //Write html string for each answer.
-  let answers;
-  answerArr.map((answer )=> answers += `
-  <li>${answer}</li>
-  `).join();
+  //Write html string for each answer to feed into questionHtml.
+  let answers = '';
+  let i = 1;
+  answerArr.map( (answer) => {
+    answers += `
+        <input type='radio' name='option' class='answer-selector' id='answer${i}' value='${answer}'><label for='answer${i}'>${answer}</label><br/>
+    `;
+    i++;
+  }).join();
   return answers;
 }
 
+// restore submit button after disabling it for next questions
+function submitReady() {
+  $('input[name=option]').on('click', function(event) {
+    $('input[name=option]:checked').addClass('selected');
+    $('.submitAnswer').removeAttr('disabled');
+    console.log('foo');
+  });
+}
+
+
 //next question
 function nextQuestion(){
-  let question = generateQuestionHtml(questionNum);
-  $('main').append(question);
-  ++questionNum;
-  return question;
-  console.log(questionNum);
+  //Remove the current question.
+  //If not last question, get next question; else render results.
+  if (questionNum < morbidQuestions.length - 1) {
+    //Generate new question html based on questionNum.
+    let question = generateQuestionHtml(questionNum);
+    //Append to main element.
+    $('main').html(question);
+    //Increment questionNum so it calls next question.
+    ++questionNum;
+    return question;
+  } else {
+    $('main').html('<h3>renderResults() content will go here</h3>');
+  }
+}
+
+/* -- ANSWER EVALUATION -- */
+
+// submit selected answer
+function submitAnswer() {
+  let userAnswer = $('input[name="option"]:checked').val();
+  $('.submitAnswer').click(function(event) {
+    event.preventDefault();
+    evaluateUserAnswers();
+    $('.nextQuestion').show();    
+    $('.submitAnswer').remove();
+  });
 }
 
 $('nextButton').click(event => {
-  nextQuestion();
+  $(main).html(nextQuestion);
 });
 
-//check if answer is correct/incorrect
-function userAnswerCorrect(){
-  
-}
+// //check if answer is correct/incorrect, display correct answer and updated score
 
-function userAnswerIncorrect(){
+function evaluateUserAnswers() {
+  let correctAnswer = morbidQuestions[questionNum].correctAnswer;
+  if (correctAnswer === userAnswer) {
+    userScore.correct++;
+    $('.feedbackCorrect').hide(); 
+  } else {
+    userScore.incorrect++;
+    getCorrectAnswer();
+    $('.feedbackIncorrect').hide();
+    $('.historyNonfan').hide(); 
+  }
+  $('.resultsCounter').html(`<p>Correct: ${userScore.correct} | Incorrect: ${userScore.incorrect}</p>`);
+}  
 
-}
+// //update score
+// function updateScore(){
 
-//update score
-function updateScore(){
-  let score = 0;
+// }
 
-}
+// //generate feedback
+// function userFeedbackCorrectAnswer(){
 
-//generate feedback
-function userFeedbackCorrectAnswer(){
+// }
 
-}
+// function userFeedbackIncorrectAnswer(){
 
-function userFeedbackIncorrectAnswer(){
+// }
 
-}
+// //render result
+// function renderResults(){
 
-//render result
-function renderResults(){
+// }
 
-}
-
-//restart quiz
-function restartQuiz(){
-
+//restart quiz, user clicks back to the home page 
+function restartQuiz() {
+  $('.resetButton').click( () => {
+    $('main').html(quizStart);
+    userScore = {
+      correct: 0,
+      incorrect: 0,
+    };
+  });
 }
 
 $(startQuiz());
+$(restartQuiz());
+$(submitReady());
 
